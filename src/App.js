9826fbbb1b0392f1a6fa47d9f694aa1edca058df
Caroline Sclavon <caroline.sclavon@gmail.com>
2023-10-04 12:45:16 +0200

    fix deployment

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Home from './pages/home.jsx';
 import AboutPage from './pages/aboutPage.jsx';
 import ProjectsPage from './pages/projectsPage.jsx';
 import Contact from './components/Contact.jsx';
+import packagejson from '../package.json';
 
 function App() {
   return (
-    <Router>
+    <Router basename={packagejson.homepage}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutPage />} />
